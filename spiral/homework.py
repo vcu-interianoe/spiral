20020e888b655686c1c3e27f5efbc65f0ddc78b7
eddy <interianoe@vcu.edu>
2021-04-08 20:21:42 -0400

    hw

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,4 +1,16 @@
 def spiralize(number):
-    return_value = 1
+    x = 1
+    y = 2
+    step = 0
+    running_total = 0   
+    while x <= (number**2):
+      
+        running_total += x
+        x += y
+        step += 1
+        if step == 4:
+            y += 2
+            step = 0    
+    return running_total
 
-    return return_value
+print(spiralize(501)) 
